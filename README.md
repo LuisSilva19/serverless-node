9cbf58b0bcbf11946596bb62f122c7be4a22e0e6
Luis <junior_mrv@live.com>
2023-09-18 10:57:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Node.js com serverless framework
+## Executando localmente
+
+O projeto foi feito da forma mais simples possível para facilitar o uso. Não foram usados nenhum framework ou biblioteca para o front-end.
+
+Todo o projeto roda a partir do arquivo `index.js`, que é o arquivo principal do projeto. Para executá-lo, basta rodar o comando:
+
+```bash
+node index.js
+```
+
+Ou se preferir, você pode usar o [nodemon](https://www.npmjs.com/package/nodemon) para executar o projeto:
+
+```bash
+npm run dev
+
+```
---
+++ api/db/database.js
@@ -0,0 +1,48 @@
+const { MongoClient, ObjectId } = require('mongodb')
+
+const connectionInstance = null
+
+async function connectToDatabase() {
+  if (connectionInstance) return connectionInstance
+  const client = new MongoClient(process.env.MONGODB_CONNECTIONSTRING)
+  const connection = await client.connect()
+  return connection.db(process.env.MONGODB_DB_NAME)
+}
+
+async function getUserByCredentials(username, password) {
+  const client = await connectToDatabase()
+  const collection = client.collection('users')
+  const user = await collection.findOne({ name: username, password: password })
+  if (!user) return null
+  return user
+}
+
+async function saveResultToDatabase(result) {
+  const client = await connectToDatabase()
+  const collection = client.collection('results')
+  const { insertedId } = await collection.insertOne(result)
+  return insertedId
+}
+
+async function getResultById(id) {
+  const client = await connectToDatabase()
+  const collection = client.collection('results')
+  const result = await collection.findOne({ _id: new ObjectId(id) })
+  if (!result) return null
+  return result
+}
+
+async function createUser(username, password){
+  const client = await connectToDatabase()
+  const collection = client.collection('users')
+  const { insertedId } = await collection.insertOne({ name: username, password: password })
+  if (!insertedId) return null
+  return insertedId
+}
+
+module.exports = {
+  getUserByCredentials,
+  saveResultToDatabase,
+  getResultById,
+  createUser
+}
